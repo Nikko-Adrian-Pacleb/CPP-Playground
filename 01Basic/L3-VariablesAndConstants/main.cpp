14df77a9c9a495b23bcf331877369bba25cd5434
Nikko-Adrian-Pacleb <pacleb.nikko@gmail.com>
2023-05-15 21:13:37 -0700

    const and constexpr trial

--- 01Basic/L3-VariablesAndConstants/main.cpp
+++ 01Basic/L3-VariablesAndConstants/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+constexpr int addItems(int x, int y) {
+        return x + y;
+    }
+
 int main() {
     int y {20};
     int x = 10;
     int z;
+    auto autovariable = true;
+    std::cout << "Value of autovariable:" << autovariable << std::endl;
     std::cout << "Value of y:" << y << std::endl;
     std::cout << "Value of x:" << x << std::endl;
     std::cout << "Enter value of z:";
@@ -11,5 +17,7 @@ int main() {
     std::cout << x << std::endl;
     std::cout << y << std::endl;
     std::cout << z << std::endl;
+    constexpr int constantexprvariable = addItems(1, 2);
+    std::cout << constantexprvariable << std::endl;
     return 0;
 }
