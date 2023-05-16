25a2b8de67d2f3d443305bc64c6d20fb6d789f9b
Nikko-Adrian-Pacleb <pacleb.nikko@gmail.com>
2023-05-15 21:22:43 -0700

    Problem 1480 Finished

--- leetcode/TheLeetCodeBeginnersGuide/1480-RunningSumOf1DArray/main.cpp
+++ leetcode/TheLeetCodeBeginnersGuide/1480-RunningSumOf1DArray/main.cpp
@@ -1,6 +1,32 @@
 #include <bits/stdc++.h>
 
+void solution(int values[], int size) {
+    // Solution goes here
+    
+    // variables
+    int sum = 0; // sum of values in index 0 to i
+    std::vector<int> newArray; // new array to store the running sum
+
+    for(int i = 0; i < size; ++i) {
+        newArray.push_back(sum + values[i]);
+        sum += values[i];
+        std::cout << newArray[i] << " ";
+    }
+}
+
 int main() {
-    std::cout << "Hello World";
+    // Number of values
+    int n;
+    std::cin >> n;
+    int arr[n];
+    
+    // Input values
+    for(int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }
+
+    // Call solution function
+    solution(arr, n);
+
     return 0;
 }
