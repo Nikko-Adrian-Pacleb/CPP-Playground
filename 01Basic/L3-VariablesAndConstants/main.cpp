4488a446d15f58c384f283b21fdf98bf8a388ff8
Nikko-Adrian-Pacleb <pacleb.nikko@gmail.com>
2023-05-15 18:48:09 -0700

    Cin Trial

--- 01Basic/L3-VariablesAndConstants/main.cpp
+++ 01Basic/L3-VariablesAndConstants/main.cpp
@@ -3,7 +3,13 @@
 int main() {
     int y {20};
     int x = 10;
+    int z;
+    std::cout << "Value of y:" << y << std::endl;
+    std::cout << "Value of x:" << x << std::endl;
+    std::cout << "Enter value of z:";
+    std::cin >> z;
     std::cout << x << std::endl;
     std::cout << y << std::endl;
+    std::cout << z << std::endl;
     return 0;
 }
