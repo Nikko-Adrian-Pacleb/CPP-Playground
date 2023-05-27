5f579c049d9d69b400227d6f1aa9331b504d88ce
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-05-27 11:28:43 -0700

    L4-MultiDimensionalArrays

--- 01Basic/L4-ArraysAndStrings/main.cpp
+++ 01Basic/L4-ArraysAndStrings/main.cpp
@@ -35,5 +35,17 @@ int main() {
     modifyArray[0] = 10;
     std::cout << modifyArray[0] << std::endl;
 
+    // # Multidimensional Arrays
+    // ## Declaring and Initializing Multidimensional Arrays
+    // ElementType arrayName[constant_number of rows][constant_number of columns] = {optional initial values};
+    std::cout << "Multidimensional Arrays" << std::endl;
+    int multiArray[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    std::cout << multiArray[0][0] << std::endl;
+    std::cout << multiArray[0][1] << std::endl;
+    std::cout << multiArray[0][2] << std::endl;
+    std::cout << multiArray[1][0] << std::endl;
+    std::cout << multiArray[1][1] << std::endl;
+    std::cout << multiArray[1][2] << std::endl;
+
     return 0;
 }
