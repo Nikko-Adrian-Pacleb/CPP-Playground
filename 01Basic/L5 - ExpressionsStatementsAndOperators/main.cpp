9aa06555be95d51d0b8d70b97a0c582ecdeb52a1
Nikko Adrian Pacleb <pacleb.nikko@gmail.com>
2023-06-16 23:19:04 -0700

    Lesson 5 started

---
+++ 01Basic/L5 - ExpressionsStatementsAndOperators/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() 
+{
+    // # Statements
+    // Statements are instructions that perform some action and do not return a value.
+    // ## Expression Statements
+    // An expression statement is an expression followed by a semicolon.
+    // ## Compound Statements
+    // A compound statement (also known as a block) is a sequence of statements surrounded by braces.
+
+    // Three-Way Comparison Operator
+    // The three-way comparison operator (also known as the spaceship operator) is a new operator in C++20.
+    // std::cout << "Three-Way Comparison Operator" << std::endl;
+    // int x = 5;
+    // int y = 6;
+    // std::cout << (x <=> y) << std::endl;
+    // std::cout << (y <=> x) << std::endl;
+    // std::cout << (x <=> x) << std::endl;
+    
+    return 0;
+}
